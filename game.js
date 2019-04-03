8fe628edb1476a0459c20492c3aafe8e83c466bc
Natasha R. Silva <natashairsilva@gmail.com>
2019-04-03 12:32:31 +0100

    Animation starts at play, skip line on enter keydown

--- game.js
+++ game.js
@@ -10,83 +10,91 @@ let newSong = new Lyrics()
 
 var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'vWwgrjjIMXA',
-          events: {
+// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'vWwgrjjIMXA',
+        events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-    //   // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        //event.target.playVideo();
-        if (event.data == YT.PlayerState.PLAYING) {
-            // incomplete lyrics start showing
-            
-        } else if (event.data === YT.PlayerState.PAUSED) {
-           // stop lyrics
-        } else if (event.data === YT.PlayerState.ENDED) {
-
         }
+    });
+}
+
+// YOUTUBE VIDEO
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    //event.target.playVideo();
+    if (event.data == YT.PlayerState.PLAYING) {
+        // incomplete lyrics start showing
+
+    } else if (event.data === YT.PlayerState.PAUSED) {
+        // stop lyrics
+    } else if (event.data === YT.PlayerState.ENDED) {
+
     }
+}
 
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        // incomplete lyrics start showing
+        animation()
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING) {
-            // incomplete lyrics start showing
-            
-        } else if (event.data === YT.PlayerState.PAUSED) {
-           // stop lyrics
-        } else if (event.data === YT.PlayerState.ENDED) {
+    } else if (event.data === YT.PlayerState.PAUSED) {
+        // stop lyrics
+    } else if (event.data === YT.PlayerState.ENDED) {
 
-        }
-      }
-
-      function stopVideo() {
-        player.stopVideo();
-      }
-
-    function animation(){
-        updateEverything();
-        drawLyrics(ctx);
-        window.requestAnimationFrame(animation);
     }
-    animation()
-      
-    function drawLyrics(ctx) {
-        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        newSong.draw(ctx);
-        }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function animation() {
+    updateEverything();
+    drawLyrics(ctx);
+    window.requestAnimationFrame(animation);
+}
+
+function drawLyrics(ctx) {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    newSong.draw(ctx);
+}
+
+function updateEverything() {
+    frame++;
+    newSong.update()
+}
+
+document.onkeydown = event => {
+    let key = event.key
     
-    function updateEverything() {
-        frame++;
-        newSong.update()
+    if (key === "Backspace") {
+        newSong.currentText = newSong.currentText.substr(0, newSong.currentText.length-1)
+    }
+    if (key.length === 1) {
+        newSong.addCharacter(event.key)
     }
 
-    document.onkeydown = event => {
-        if (event.keyCode === 13) { // enter
-        // when ENTER key pressed 
-            // check input value
-            // detect if it is the correct word
-            newSong.check();
-            // increase points
-            // video resume
-            // else
-            // just resume video
-          }
-      }      
+    if (event.keyCode === 13) { // enter
+        newSong.missingWordsIndex++;
+        newSong.currentLyricIndex++;
+        newSong.correctAnswers.push(false);
+    }
+}
+
+        // increase points
+        // resume video
+        // else
+        // just resume video
--- lyrics.js
+++ lyrics.js
@@ -4,48 +4,88 @@ class Lyrics {
         this.height = CANVAS_HEIGHT;
         this.y = 0;
         this.missingIcon = "♫";
+        // this.missingIcon = "______";
         this.missingWords = ["roads", "call", "dove", "sleeps", "cannon", "banned", "blowin", "answer", "mountain",
         "washed", "some", "allowed", "turn", "pretend", "wind", "blowin", "look", "sky",
         "ears", "people", "deaths", "many", "friend", "wind"],
         this.lyrics = [`How many ${this.missingIcon} must a man walk down`, `Before you ${this.missingIcon} him a man?`,
-            `How many seas must a white ${this.missingIcon} sail`, `Before she ${this.missingIcon} in the sand?`, `Yes, 'n' how many times must the ${this.missingIcon} balls fly`,
-            `Before they're forever ${this.missingIcon}?`, `The answer, my friend, is ${this.missingIcon}' in the wind`, `The ${this.missingIcon} is blowin' in the wind`,
-            `Yes, 'n' how many years can a ${this.missingIcon} exist`, `Before it's ${this.missingIcon} to the sea?`, `Yes, 'n' how many years can ${this.missingIcon} people exist`,
-            `Before they're ${this.missingIcon} to be free?`, `Yes, 'n' how many times can a man ${this.missingIcon} his head`, `And ${this.missingIcon} that he just doesn't see?`,
-            `The answer, my friend, is blowin' in the ${this.missingIcon}`, `The answer is ${this.missingIcon}' in the wind`, `Yes, 'n' how many times must a man ${this.missingIcon} up`,
-            `Before he can see the ${this.missingIcon}?`, `Yes, 'n' how many ${this.missingIcon} must one man have`, `Before he can hear ${this.missingIcon} cry?`, 
-            `Yes, 'n' how many ${this.missingIcon} will it take till he knows`, `That too ${this.missingIcon} people have died?`, `The answer, my ${this.missingIcon}, is blowin' in the wind`,
-            `The answer is blowin' in the ${this.missingIcon}`]
+        `How many seas must a white ${this.missingIcon} sail`, `Before she ${this.missingIcon} in the sand?`, `Yes, 'n' how many times must the ${this.missingIcon} balls fly`,
+        `Before they're forever ${this.missingIcon}?`, `The answer, my friend, is ${this.missingIcon}' in the wind`, `The ${this.missingIcon} is blowin' in the wind`,
+        `Yes, 'n' how many years can a ${this.missingIcon} exist`, `Before it's ${this.missingIcon} to the sea?`, `Yes, 'n' how many years can ${this.missingIcon} people exist`,
+        `Before they're ${this.missingIcon} to be free?`, `Yes, 'n' how many times can a man ${this.missingIcon} his head`, `And ${this.missingIcon} that he just doesn't see?`,
+        `The answer, my friend, is blowin' in the ${this.missingIcon}`, `The answer is ${this.missingIcon}' in the wind`, `Yes, 'n' how many times must a man ${this.missingIcon} up`,
+        `Before he can see the ${this.missingIcon}?`, `Yes, 'n' how many ${this.missingIcon} must one man have`, `Before he can hear ${this.missingIcon} cry?`, 
+        `Yes, 'n' how many ${this.missingIcon} will it take till he knows`, `That too ${this.missingIcon} people have died?`, `The answer, my ${this.missingIcon}, is blowin' in the wind`,
+        `The answer is blowin' in the ${this.missingIcon}`]
+        this.currentLyricIndex = 0
+        this.missingWordsIndex = 0
+        this.currentText = ""
+        this.correctAnswers = [] // If [true,true,false], it means that the 2 lines were correct and not the 3rd line
     }
 
     draw(ctx) {
         ctx.save();
-        ctx.fillStyle = 'black';
-        ctx.font = "15px Arial";
+        ctx.font = "18px Arial";
         ctx.textAlign = "center";
         for (let i = 0, j = this.y; i < this.lyrics.length; i++, j-=25){
-        ctx.fillText(this.lyrics[i], CANVAS_WIDTH-250, CANVAS_HEIGHT-j);
+            if (i < this.currentLyricIndex) {
+               if (this.correctAnswers[i]) {
+                    ctx.fillStyle = 'green';
+               }
+               else {
+                    ctx.fillStyle = 'red';
+               }
+            }
+            else if (i === this.currentLyricIndex) {
+                ctx.fillStyle = 'blue';
+            }
+            else {
+                ctx.fillStyle = 'black';
+            }
+            let typedWord
+            
+            // The first lyrics, the missing word is displayed
+            if (i < this.currentLyricIndex) {
+                typedWord = this.missingWords[i]
+            }
+            // The current lyric, the typed word and "_" is displayed
+            else if (i === this.currentLyricIndex) {
+                typedWord = this.currentText + "_".repeat(this.missingWords[i].length - this.currentText.length)
+            }
+            // The last lyrics, "_" is displayed
+            else {
+                typedWord = "_".repeat(this.missingWords[i].length)
+            }
+            let text = this.lyrics[i]
+            text = text.replace(this.missingIcon, typedWord)
+            ctx.fillText(text, CANVAS_WIDTH/2, CANVAS_HEIGHT-j);
         }
         ctx.restore();
     }
 
-    check() {
-        this.lyrics.forEach(element, i => {
-                if ($input.value === this.missingWords[i]){
-                    this.missingIcon = $inputValue;
-                    return true;    
-                } else {
-                return false;
-                    }
-                }
-        );
+    addCharacter(character){
+        this.currentText += character
+        if (this.currentText.length === this.missingWords[this.currentLyricIndex].length) {
+            if (this.currentText === this.missingWords[this.missingWordsIndex]) {
+                console.log("YES!")
+                this.correctAnswers.push(true)
+            }
+            else {
+                console.log("NO!")
+                this.correctAnswers.push(false)
+            }
+            this.missingWordsIndex++
+            this.currentLyricIndex++
+            this.currentText = ""
+        }
     }
     
     update() {
         if (true) {
-            this.y += 0.5;
+            this.y += 0.1;
         } else {
             // stop
+            this.y;
         }
     }
 }   
